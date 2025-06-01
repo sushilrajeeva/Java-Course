87ad1946a199a4b34487c852ea0324057856f9d2
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-06-01 01:15:56 -0400

    List

---
+++ Java-Advanced-Collections/Java_Collections/src/com/coding/collection/TestingList.java
@@ -0,0 +1,23 @@
+package com.coding.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestingList {
+
+    public static void main(String[] args) {
+        List<String> strList = new ArrayList<String>();
+        strList.add("Sushil");
+        strList.add("Nikhil");
+        strList.add("Jaswant");
+
+        for (String s : strList) {
+            System.out.printf("Name : %s \n", s);
+        }
+
+        if (strList.contains("Jaswant")) {
+            System.out.printf("Jaswant exist in the list at index : %d \n", strList.indexOf("Jaswant"));
+        }
+    }
+
+}
